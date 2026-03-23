645ab0981562533a17f7046033d19e176f508ba1
Krunal Shah <krunal@infynno.com>
2026-03-23 18:38:35 +0530

    feat: initial commit

--- lib/layout.shared.tsx
+++ lib/layout.shared.tsx
@@ -1,16 +1,16 @@
-import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
 
 // fill this with your actual GitHub info, for example:
 export const gitConfig = {
-  user: 'fuma-nama',
-  repo: 'fumadocs',
-  branch: 'main',
+  user: "krunal-infynno",
+  repo: "engineering-handbook",
+  branch: "main",
 };
 
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
-      title: 'My App',
+      title: "Engineering Handbook",
     },
     githubUrl: `https://github.com/${gitConfig.user}/${gitConfig.repo}`,
   };
