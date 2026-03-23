ac28305615009807cd75bdc2687dc147ca917cbf
Krunal Shah <krunal@infynno.com>
2026-03-23 19:13:57 +0530

    feat: update layout and documentation structure with new styles and quick start guide

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -1,10 +1,19 @@
 import { source } from "@/lib/source";
 import { DocsLayout } from "fumadocs-ui/layouts/docs";
 import { baseOptions } from "@/lib/layout.shared";
+import type { CSSProperties } from "react";
 
 export default function Layout({ children }: LayoutProps<"/docs">) {
+  const containerStyle = {
+    "--fd-layout-width": "100%",
+  } as CSSProperties;
+
   return (
-    <DocsLayout tree={source.getPageTree()} {...baseOptions()}>
+    <DocsLayout
+      tree={source.getPageTree()}
+      containerProps={{ style: containerStyle }}
+      {...baseOptions()}
+    >
       {children}
     </DocsLayout>
   );
