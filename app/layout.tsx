1147f5ccbb9008ea0725b8fdc7f162a45b9814de
Krunal Shah <krunal@infynno.com>
2026-03-24 10:59:01 +0530

    feat: enhance layout metadata and update documentation with new icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,19 @@
 import { RootProvider } from "fumadocs-ui/provider/next";
 import "./global.css";
 import { Inter } from "next/font/google";
+import type { Metadata } from "next";
 
 const inter = Inter({
   subsets: ["latin"],
 });
+
+export const metadata: Metadata = {
+  icons: {
+    icon: "/infynno-solutions-llp.png",
+    shortcut: "/infynno-solutions-llp.png",
+    apple: "/infynno-solutions-llp.png",
+  },
+};
 
 export default function Layout({ children }: LayoutProps<"/">) {
   return (
